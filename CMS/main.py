944991694a045c27b1769ce619c4040956c22abe
AKM_Machine <anindya15l12@gmail.com>
2021-09-05 22:07:29 +0530

    Did absolutely nothing here, add logic :)

--- CMS/main.py
+++ CMS/main.py
@@ -2,7 +2,7 @@
 # local packages
 from detection import detect
 from utilities import triangulate, navigate, draw_vtm, draw_pts, draw_bot, draw_rts, display_msg, remove_vtm
-# import mqtt_router
+import mqtt_router
 # global packages
 import numpy as np
 import cv2
@@ -11,4 +11,14 @@ import requests
 
 from multiprocessing.dummy import Pool
 
+
+# functions
+def on_exit():
+    # TODO - add on exit event logic
+    mqtt_router.client.loop_stop()
+
+
 # write the actual logic here
+if __name__ == '__main__':
+    # TODO - add logic
+    pass
