5428b0af7fed844f8462c39c751df45c5c657546
nightKnight <76892920+nightKnight112@users.noreply.github.com>
2021-09-30 10:36:56 +0530

    Updated aruco_pos_finder.py
    Fixed logic and optimized ending

--- CMS/ArUco/aruco_pos_finder.py
+++ CMS/ArUco/aruco_pos_finder.py
@@ -1,57 +1,69 @@
-#4 dim arrays
+# 4 dim arrays
 import cv2
 import time
 import sys
 
-#ids ar 2d arrays
-#might be req to take the below part within the function
+# ids ar 2d arrays
+# might be req to take the below part within the function
 dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
 parameters = cv2.aruco.DetectorParameters_create()
-#upto here
+# upto here
 
-def id_fetcher_initial(frame):  #run initially to fetch start and end points
-    start_point_coords = []
-    end_point_coords = []
-    t_strt = 0
-    t_end = 0
+class id_storage(dict):
+    def __init__(self):
+        self = dict()
+
+    def element_adder(self, key, value):
+        self[key] = value
+
+
+#rewrote using dictionary and updated logic so as duplicate values for same ids don't cause trouble
+#it will return 2 dicts, with keys as id number and values as their centre coords
+def id_fetcher_initial(frame):
+    strt_pnts_coords = id_storage()
+    end_pnts_coords = id_storage()
+
+    # t_strt, t_end, i, j = 0, 0, 0, 0
+
     try:
         markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
         for [ids] in markerIds:
-            if t_strt == 4 and t_end == 4:
+            if len(strt_pnts_coords) == 4 and len(end_pnts_coords) == 4:
                 break
+            elif ids in range(680, 685):
+                x, y = int(markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, int(
+                    markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
+                end_pnts_coords[ids] = tuple(x, y)
+            elif ids in range(830, 835):
+                x, y = int(markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, int(
+                    markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
+                strt_pnts_coords[ids] = tuple(x, y)
             else:
-                if ids in range(680, 685):  #D1, D2, D3, D4
-                    x, y = (markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, (markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
-                    end_point_coords.append(tuple(x, y))   #centre coords
-                    t_strt += 1
-                elif ids in range(830, 835):   #S1, S2, S3, S4
-                    x, y = (markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, (markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
-                    start_point_coords.append(tuple(x, y))  #centre coords
-                    t_end += 1
-                else:
-                    print("Wrong id detected")
-        return start_point_coords, end_point_coords  #returning an array containing (x, y) of start and end points
+                continue
+
+        return strt_pnts_coords, end_pnts_coords
 
     except:
-        time.sleep(0.25)  #code will sleep for this amount of secs if frame not found
+        time.sleep(0.25)
         markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
         for [ids] in markerIds:
-            if t_strt == 4 and t_end == 4:
+            if len(strt_pnts_coords) == 4 and len(end_pnts_coords) == 4:
                 break
+            elif ids in range(680, 685):
+                x, y = int(markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, int(
+                    markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
+                end_pnts_coords[ids] = tuple(x, y)
+            elif ids in range(830, 835):
+                x, y = int(markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, int(
+                    markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
+                strt_pnts_coords[ids] = tuple(x, y)
             else:
-                if ids in range(680, 685):  # D1, D2, D3, D4
-                    x, y = (markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, (
-                                markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
-                    end_point_coords.append(tuple(x, y))  # centre coords
-                    t_strt += 1
-                elif ids in range(830, 835):  # S1, S2, S3, S4
-                    x, y = (markerCorners[0][0][0][0] + markerCorners[0][0][2][0]) // 2, (
-                                markerCorners[0][0][0][1] + markerCorners[0][0][2][1]) // 2
-                    start_point_coords.append(tuple(x, y))  # centre coords
-                    t_end += 1
-                else:
-                    print("Wrong id detected")
-        return start_point_coords, end_point_coords  # returning an array containing (x, y) of start and end points
+                continue
+
+        return strt_pnts_coords, end_pnts_coords
+
     finally:
-        print("no frame found after waiting")
-        sys.exit()
+        print("No frame provided even after waiting")
+        return 0, 0   #for inclusion in main, if return values are 0 rerun process
+        #sys.exit()
+    
