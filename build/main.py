535a62e0c78a14163042e4275f2bd30078b879d5
Archisman Das <archisman112358@gmail.com>
2021-10-03 11:04:12 +0530

    last day...mew mew

--- build/main.py
+++ build/main.py
@@ -7,7 +7,7 @@ from mqtt_router import control, on_exit
 import utils
 
 # camera initialise
-frame_timing = 1 # in milliseconds
+frame_timing = 1  # in milliseconds
 cap = cv.VideoCapture(0, cv.CAP_DSHOW)
 cap.set(cv.CAP_PROP_FPS, 30.0)
 cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter.fourcc('m', 'j', 'p', 'g'))
@@ -15,11 +15,11 @@ cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter.fourcc('M', 'J', 'P', 'G'))
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
-#ArUco variables init
+# ArUco variables init
 dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_1000)
 parameters = cv.aruco.DetectorParameters_create()
 
-#Globals
+# Globals
 Induct_Dist_Thres = 20
 Bot_Angle_Thres = 20
 Speed_pwm = 200
@@ -32,11 +32,12 @@ def rescaleFrame(frame, scale=1):
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
+
 # Dictionary to store the ids and coordinates
 Point = dict()
 # Arena confirmation Loop
 # Loop 1
-#xys = set()
+# xys = set()
 while True:
     ret, frm = cap.read()
 
@@ -50,29 +51,28 @@ while True:
 
     # Detect the markers in the image
     markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(frm, dictionary, parameters=parameters)
-    if (markerIds is not None):
+    if markerIds is not None:
         for a in markerIds:
             cc = np.where(markerIds == a[0])
             c = int(cc[0][0])
             s = str(Inducts.get_name(a[0]))
             Point[a[0]] = utils.find_coordinates(markerCorners, c)
-            (x,y) = Point[a[0]][1]
+            (x, y) = Point[a[0]][1]
             cv.putText(frm,
-                    s, (x, y),
-                    cv.FONT_HERSHEY_PLAIN,
-                    1, (255, 0, 255),
-                    thickness=1)
+                       s, (x, y),
+                       cv.FONT_HERSHEY_PLAIN,
+                       1, (255, 0, 255),
+                       thickness=1)
             center = Point[a[0]][0]
             cv.circle(frm, center, 4, (0, 0, 255), -1)
-
 
         # Logic for showing paths
         if set(Inducts.SD.keys()).issubset(Point.keys()):
             for i in range(0, 4):
-                S = Point[Inducts.key_list[i]][0] # S point
-                D = Point[Inducts.key_list[i+4]][0] # D point
-                M = (S[0], D[1]) # Mid point
-                #xys.add(M)
+                S = Point[Inducts.key_list[i]][0]  # S point
+                D = Point[Inducts.key_list[i + 4]][0]  # D point
+                M = (S[0], D[1])  # Mid point
+                # xys.add(M)
                 cv.circle(frm, M, 4, (0, 0, 255), -1)
                 cv.line(frm, S, M, (164, 73, 163), 2)
                 cv.line(frm, D, M, (164, 73, 163), 2)
@@ -95,11 +95,11 @@ while True:
 
 # Confirmation Snapshot
 cv.putText(fr,
-            "Press any key, after you have placed all the bots!",
-            (10, 50),
-            cv.FONT_HERSHEY_PLAIN,
-            2.5, (255, 0, 255),
-            thickness=2)
+           "Press any key, after you have placed all the bots!",
+           (10, 50),
+           cv.FONT_HERSHEY_PLAIN,
+           2.5, (255, 0, 255),
+           thickness=2)
 cv.imshow('Image', rescaleFrame(fr))
 cv.waitKey(0)
 cv.destroyAllWindows()
@@ -117,7 +117,7 @@ while True:
     # Detect the markers in the image
     markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(
         frm, dictionary, parameters=parameters)
-    if (markerIds is not None):
+    if markerIds is not None:
         for a in markerIds:
             cc = np.where(markerIds == a[0])
             c = int(cc[0][0])
@@ -142,19 +142,19 @@ while True:
                         Location[id] = Bots.get_id(k)
 
     cv.putText(frm, "Press 'p' to start execution!",
-            (10, 50),
-            cv.FONT_HERSHEY_PLAIN,
-            2.5, (255, 0, 255),
-            thickness=2)
+               (10, 50),
+               cv.FONT_HERSHEY_PLAIN,
+               2.5, (255, 0, 255),
+               thickness=2)
     cv.aruco.drawDetectedMarkers(frm, markerCorners)
     cv.imshow("Frame", rescaleFrame(frm))
 print("Locations")
-print(Location) # GGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+print(Location)  # GGGGGGGGGGGGGGGGGGGGGGGGGGGGG
 
 # on_exit()
 # sys.exit()
 # flags initialisation
-n:int = 0
+n: int = 0
 err_flag = False
 isVertical = True
 isReturn = False
@@ -164,77 +164,88 @@ pwm_b = Speed_pwm
 while True:
     ret, frm = cap.read()
 
-    Sx = Inducts.key_list[n] # ArUco for Source Induct
+    Sx = Inducts.key_list[n]  # ArUco for Source Induct
     Dx = Inducts.key_list[n + 4]  # ArUco for destination Induct
-    S_cnt = Point[Sx][0] # center point of source induct
+    S_cnt = Point[Sx][0]  # center point of source induct
     D_cnt = Point[Dx][0]  # center point of destination induct
     # exit sequence
     if cv.waitKey(1) & 0xff == ord('q') or n >= 4 or Sx not in Location.keys():
         for ids in Bots.val_list:
-            control(ids, 3, direction = 0, pwm = 0)
+            control(ids, 3, direction=0, pwm=0)
         break
-    id = Location[Sx] # get the bot Id placed on the particular source induct
-    num = Bots.get_num(id) # ArUco number of that bot
+    id = Location[Sx]  # get the bot Id placed on the particular source induct
+    num = Bots.get_num(id)  # ArUco number of that bot
 
     # Detect the markers in the image
     markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(frm, dictionary, parameters=parameters)
-    if (markerIds is not None):
+    if markerIds is not None:
         for a in markerIds:
             cc = np.where(markerIds == a[0])
             c = int(cc[0][0])
-            if a[0] == num: # if the detected id is the bot of interest
+            if a[0] == num:  # if the detected id is the bot of interest
                 cofbot, left_top, _ = utils.find_coordinates(markerCorners, c)
                 s = str(Bots.get_id(a[0]))
                 cv.putText(frm, s, left_top, cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), thickness=2)
                 cv.circle(frm, cofbot, 2, (0, 0, 255), -1)
+
                 right_top = int(markerCorners[c][0][1][0]), int(markerCorners[c][0][1][1])
+
                 fofbot = utils.mid_pt(left_top, right_top)
-                cv.arrowedLine(frm, cofbot, fofbot, (50,255,128), 2)
+                cv.arrowedLine(frm, cofbot, fofbot, (50, 255, 128), 2)
                 mid = utils.std_v(S_cnt, D_cnt)
+
                 if isVertical and not isReturn:
-                    normal = utils.std_v(S_cnt, cofbot)
+                    normal = utils.std_v(S_cnt, cofbot)  #perpendicular
                     dist_normal = utils.dist(cofbot, normal)
                     cv.line(frm, cofbot, normal, (255, 0, 0), 2)
-                    dist_dest = utils.dist(cofbot, mid) # dist. from bot to turn point
+
+                    dist_dest = utils.dist(cofbot, mid)  # dist. from bot to turn point
                     _, angle = utils.anglechecker(cofbot, fofbot, mid)
-                    #Bot should move forward
-                    control(id , 3 , direction =1) 
+                    # Bot should move forward
+                    if dist_normal < Induct_Dist_Thres:
+                      control(id, 3, direction=1, pwm=175)
 
-                    x2 = cofbot[0]   
+                    x2 = cofbot[0]
                     x1 = S_cnt[0]
+                    print(x2-x1)
 
-                    if x2 - x1> 0:
+                    if x2 - x1 > 0:
                         print("Left higher")
-                        control(id,1, pwm=pwm_l+Pro_con*dist_normal)
+                        control(id, 3, direction=0)
+                        control(id, 2, direction=1)
+                        #control(id, 1, pwm=pwm_l + Pro_con * dist_normal)
 
                     elif x2 - x1 < 0:
                         print("Right higher")
                         control(id, 2, pwm=pwm_r + Pro_con * dist_normal)
 
                     if dist_dest < Induct_Dist_Thres:
-                        isVertical = False   #eikhane ekta control(id, 3, direction=0)
+                        #isVertical = False  # eikhane ekta control(id, 3, direction=0)
 
-                        #isVertical ke if else er moddhe dhukiye False kor
+                        # isVertical ke if else er moddhe dhukiye False kor
                         if 0 <= n <= 1:
                             print("Bot turns right")
-                            control(id,2, direction=1,pwm=pwm_r)
-                            control(id,1, direction=2,pwm=pwm_l)
+                            control(id, 2, direction=1, pwm=pwm_r)
+                            control(id, 1, direction=2, pwm=pwm_l)
+                            if angle>(90-Bot_Angle_Thres):
+
                         else:
                             print("Bot turns left")
-                            control(id,1, direction=1,pwm=pwm_r)
-                            control(id,2, direction=2,pwm=pwm_l)
+                            control(id, 1, direction=1, pwm=pwm_r)
+                            control(id, 2, direction=2, pwm=pwm_l)
 
-                # end of vertical f6rward locomotion scope
+                # end of vertical forward locomotion scope
                 elif not isVertical and not isReturn:
                     normal = utils.std_h(D_cnt, cofbot)
                     cv.line(frm, cofbot, normal, (255, 0, 0), 2)
                     dist_dest = utils.dist(cofbot, D_cnt)  # dist. from bot to turn point
                     _, angle = utils.anglechecker(cofbot, fofbot, D_cnt)
-                    
-                    if (angle < Bot_Angle_Thres and dist_normal < Induct_Dist_Thres):
+                    print(angle, dist_normal)
+
+                    if angle < Bot_Angle_Thres and dist_normal < Induct_Dist_Thres:   # angle intial 90 and decreases
                         print("Bot moves forward")
                         control(id, 3, direction=0)
-                        
+
                     if dist_dest < Induct_Dist_Thres:
                         isReturn = True
                         print("Bot unloads")
@@ -270,13 +281,8 @@ while True:
                         print(n)
                 # end of vertical return locomotion scope
 
-
-
-
-
     cv.aruco.drawDetectedMarkers(frm, markerCorners)
     cv.imshow("Frame", rescaleFrame(frm))
-
 
 cap.release()
 cv.destroyAllWindows()
--- build/utils.py
+++ build/utils.py
@@ -1,89 +1,96 @@
+import math
 from math import *
 
 
-def dist(a,b):
-    #two tuples/ coordinates
-    return sqrt(((b[0]-a[0])**2)+((b[1]-a[1])**2))
+def dist(a, b):
+    # two tuples/ coordinates
+    return int(math.dist(a, b))
 
-def xyd(a,b):
-    x1,y1=a[0],a[1]
-    x2,y2=b[0],b[1]
-    x=abs(x2-x1)
-    y=abs(y2-y1)
-    return (x,y)
 
-def std_v(a,b):
+def xyd(a, b):
+    x1, y1 = a[0], a[1]
+    x2, y2 = b[0], b[1]
+    x = abs(x2 - x1)
+    y = abs(y2 - y1)
+    return (x, y)
+
+
+def std_v(a, b):
     return (a[0], b[1])
+
 
 def std_h(a, b):
     return (b[0], a[1])
 
 
-def pid(cofbot,dest):
-    cob=cofbot[0]-dest[0]
+def pid(cofbot, dest):
+    cob = cofbot[0] - dest[0]
     print(cob)
     if cob > 10:
         return 2
-    elif cob< -10:
+    elif cob < -10:
         return 1
     else:
         return 3
 
 
-def anglechecker(centre,pt1,edge):
-    (x,y) = centre
-    x1,y1=pt1[0],pt1[1]
-    x2,y2=edge[0],edge[1]
-    if (x==x1):
-        m1=1
+def anglechecker(centre, pt1, edge):
+    (x, y) = centre
+    x1, y1 = pt1[0], pt1[1]
+    x2, y2 = edge[0], edge[1]
+    if x == x1:
+        m1 = 1
     else:
-        m1=(y-y1)/(x-x1)
-    if (x==x2):
-        m2=1
+        m1 = (y - y1) / (x - x1)
+    if x == x2:
+        m2 = 1
     else:
-        m2=(y-y2)/(x-x2)
-    #print('m1:',m1,'m2:', m2)
-    if (m1==1 and abs(m2)==0) or (m2==1 and abs(m1)==0):
-        theta=90
+        m2 = (y - y2) / (x - x2)
+    # print('m1:',m1,'m2:', m2)
+    if (m1 == 1 and abs(m2) == 0) or (m2 == 1 and abs(m1) == 0):
+        theta = 90
     else:
-        rad=atan((m2-m1)/(1+(m1*m2)))
-        theta=round(degrees(rad))
+        rad = atan((m2 - m1) / (1 + (m1 * m2)))
+        theta = round(degrees(rad))
 
-    #print(theta)
+    # print(theta)
     # if theta<0:
     #     theta=-(theta)
-    if theta<=5:
+    if theta <= 5:
         return True, theta
     return False, theta
 
-def checker(dist,dist1):
-    #print(dist,dist1)
-    val=False
+
+def checker(dist, dist1):
+    # print(dist,dist1)
+    val = False
     if dist:
-        if(dist1<=50):
-            #print(dist1)
-            dist1=0
+        if (dist1 <= 50):
+            # print(dist1)
+            dist1 = 0
             dist.pop()
             print("stop")
             # dist1=dist2
             val = True
 
-        elif(dist1<dist[-1]):
+        elif (dist1 < dist[-1]):
             dist.pop()
             dist.append(dist1)
             print("forward")
 
     else:
-        #if dist1:
+        # if dist1:
         dist.append(dist1)
-        #print(dist1)
+        # print(dist1)
     #     # else:
     #     #     dist.append(dist2)
     #     #     print(dist2)
     return val
 
+
 def mid_pt(a, b):
     return (int((a[0] + b[0]) // 2), int((a[1] + b[1]) // 2))
+
 
 '''
 utilities for ArUco Marker Position calculations
@@ -92,17 +99,19 @@ returns the list containing 3 tuples
 2. Top Left Corner coodinates
 3. Bottom Right Corner coordinates
 '''
-def find_coordinates (markerCorners, c):
+
+
+def find_coordinates(markerCorners, c):
     center = (
         ((int(markerCorners[c][0][0][0]) + int(markerCorners[c][0][2][0])) // 2),
         ((int(markerCorners[c][0][0][1]) + int(markerCorners[c][0][2][1])) // 2)
-        )
+    )
     left_top_corner = (
-                int(markerCorners[c][0][0][0]),
-                int(markerCorners[c][0][0][1])
-                )
+        int(markerCorners[c][0][0][0]),
+        int(markerCorners[c][0][0][1])
+    )
     right_bottom_corner = (
-                int(markerCorners[c][0][2][0]),
-                int(markerCorners[c][0][2][1])
-                )
+        int(markerCorners[c][0][2][0]),
+        int(markerCorners[c][0][2][1])
+    )
     return [center, left_top_corner, right_bottom_corner]
